60f9900da5547d2c9fcc31358f34ba7413926e3e
Per Lönn Wege <doxxan@gmail.com>
2014-12-11 11:17:40 +0100

    Circle vs Circle collission.

--- physics/physics.go
+++ physics/physics.go
@@ -27,10 +27,36 @@ type Circle struct {
 	position vector.Vec2
 }
 
-func CirclevsCircle(a, b Circle) bool {
+func CirclevsCircle(a, b *Circle) (bool, *Manifold) {
+	// Vector from a to b
+	normal := vector.Sub2(b.position, a.position)
+
 	r := a.radius + b.radius
 	r *= r
-	return (r < math.Pow(a.position.X+b.position.X, 2)+math.Pow(a.position.X+b.position.Y, 2))
+
+	// Initial check if circles are overlapping
+	if math.Pow(a.position.X+b.position.X, 2)+math.Pow(a.position.X+b.position.Y, 2) > r {
+		return false, nil
+	}
+
+	// We have overlap, compute manifold
+	m := Manifold{}
+	/*m.a = a
+	m.b = b*/
+
+	if d := normal.Length(); d != 0.0 {
+		// Distance is difference between radius and distance
+		m.penetration = r - d
+
+		// Utilize d since we performed sqrt on it already
+		// Points from a to b and is a unit vector
+		m.normal = vector.DivScalar2(normal, d)
+	} else {
+		m.penetration = a.radius
+		m.normal = vector.Vec2{X: 1.0, Y: 0.0}
+	}
+
+	return true, &m
 }
 
 type Body struct {
@@ -88,13 +114,13 @@ func (m *Manifold) ResolveCollision() {
 }
 
 func (m *Manifold) PositionalCorrection() {
-	/*percent := 0.2
-	  slop := 0.01
+	percent := 0.2
+	slop := 0.01
 
-	  a := m.b1
-	  b := m.b2*/
+	a := m.b1
+	b := m.b2
 
-	/*Vec2 correction = max( penetration - k_slop, 0.0f ) / (A.inv_mass + B.inv_mass)) * percent * n
-	  A.position -= A.inv_mass * correction
-	  B.position += B.inv_mass * correction*/
+	correction := m.normal
+	correction.MulScalar(math.Max(m.penetration-slop, 0.0) / (a.invMass + b.invMass))
+	correction.MulScalar(percent)
 }
